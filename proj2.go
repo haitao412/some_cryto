a0d93e834d17641fcc2cd628d277289f02425bf8
Haitao <haitaozhu@berkeley.edu>
2018-10-02 11:05:55 -0700

    store and load

--- proj2.go
+++ proj2.go
@@ -1,3 +1,21 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @haitao412 Sign out
+1
+0 1 lnguyen-cpi/some_cryto
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
+some_cryto/proj2.go
+76c0fd6  10 hours ago
+@lnguyen-cpi lnguyen-cpi fix GetUser and add Decrypt
+@lnguyen-cpi @haitao412
+     
+287 lines (219 sloc)  8.7 KB
 package proj2
 
 // You MUST NOT change what you import.  If you add ANY additional
@@ -124,6 +142,19 @@ func encrypt_data(key []byte, data []byte) ([] byte) {
 	return ciphertext
 }
 
+/*
+Decrypt data with CFB block method
+append iv as first of cipher text since it 
+is not secret, just need to be random
+*/
+
+func decrypt_data(key []byte, data []byte) ([] byte){
+
+    block := userlib.CFBDecrypter(key, data[:userlib.BlockSize])
+	block.XORKeyStream(data[userlib.BlockSize:], data[userlib.BlockSize:])
+	return data[userlib.BlockSize:]
+}
+
 
 
 /***************************************************************
@@ -163,11 +194,12 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 	userdata.encryptionKey = encyption_key
 
 	user_json, _ = json.Marshal(userdata)
-	encypted_user_data = encrypt_data(encyption_key, user_json)
-	signed_encypted_user_data = userlib.RSASign(signed_key, encypted_user_data)
-	encypted_user_data_and_signature = [2][]byte{encypted_user_data, signed_encypted_user_data}
-	encypted_user_data_and_signature_json, _ = json.Marshal(encypted_user_data_and_signature)
-	userlib.DatastoreSet(string(datastore_key), encypted_user_data_and_signature_json)
+	encrypted_user_data = encrypt_data(encyption_key, user_json)
+
+	signed_encrypted_user_data = userlib.RSASign(signed_key, encrypted_user_data)
+	encrypted_user_data_and_signature = [2][]byte{encrypted_user_data, signed_encrypted_user_data}
+	encrypted_user_data_and_signature_json, _ = json.Marshal(encrypted_user_data_and_signature)
+	userlib.DatastoreSet(string(datastore_key), encrypted_user_data_and_signature_json)
 
     
 	return &userdata, err
@@ -177,35 +209,59 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 // fail with an error if the user/password is invalid, or if the user
 // data was corrupted, or if the user can't be found.
 func GetUser(username string, password string) (userdataptr *User, err error) {
+
+	publicKey, key_exist := userlib.KeystoreGet(username)
+	if !key_exist {
+		return nil, errors.New(strings.ToTitle("Not Found User !!!"))
+	}
+
 	var userdata User
 	datastore_key, encyption_key = generate_user_key(username, password)
-	publicKey = userlib.KeystoreGet(username)
-	receiveFile = DatastoreGet(datastore_key)
-	var encypted_user_data_and_signature [2][]byte
-	err := json.Unmashaled(receiveFile, &encypted_user_data_and_signature)
-	if err != nil {
-		return err
+	user_data_json, key_exist = userlib.DatastoreGet(string(datastore_key))
+	if !key_exist {
+		return nil, errors.New(strings.ToTitle("Wrong user information !!!"))
 	}
-	encypted_user_data = encypted_user_data_and_signature[0] 
-	signed_encypted_user_data = encypted_user_data_and_signature[1]
-	err := userlib.RSAVerify(publicKey, encypted_user_data, encypted_user_data_and_signature)
+
+	var encrypted_user_data_and_signature [2][]byte
+
+	err := json.Unmashaled(user_data_json, &encrypted_user_data_and_signature)
 	if err != nil {
-		return err
+		return nil, errors.New(strings.ToTitle("Json of user data corrupted !!!"))
 	}
-	user_json = decrypt_data(encyption_key, encypted_user_data)
-	err := json.Unmashaled(user_json, userdata) 
+
+	encrypted_user_data = encrypted_user_data_and_signature[0] 
+	signed_encrypted_user_data = encrypted_user_data_and_signature[1]
+
+	err := userlib.RSAVerify(&publicKey, encrypted_user_data, signed_encrypted_user_data)
 	if err != nil {
-		return err
+		return nil, errors.New(strings.ToTitle("user data corrupted !!!"))
+	}
+
+	user_json = decrypt_data(encyption_key, encrypted_user_data)
+	err := json.Unmashaled(user_json, &userdata) 
+	if err != nil {
+		return nil, errors.New(strings.ToTitle("Json of user data corrupted !!!"))
 	}
 	return &userdata, err
+
 }
 
 // This stores a file in the datastore.
 //
 // The name of the file should NOT be revealed to the datastore!
 func (userdata *User) StoreFile(filename string, data []byte) {
-}
+	filename_json,_ = json.Marshal(filename)
+	encrypt_filename = encrypt_data(userdata.encyptionKey,filename_json)
 
+	file_data_json,_ = json.Marshal(data)
+	encrypt_data_json = encrypt_data(userdata.encryptionKey, file_data_json)
+	signed_encrypted_file_data_json = userlib.RSASign(userdata.signedKey, encrypted_data_json)
+	encrypt_data_json_with_signature = [2][]byte{encrypt_data_json, signed_encrypted_file_data_json}
+	encrypt_data_json_with_signature_json,_ = json.Marshal(encrypt_data_json_with_signature)
+	userlib.DatastoreSet(string(encrypt_filename), encrypt_data_json_with_signature_json)
+	return &userdata, err
+}
+file
 // This adds on to an existing file.
 //
 // Append should be efficient, you shouldn't rewrite or reencrypt the
@@ -220,7 +276,32 @@ func (userdata *User) AppendFile(filename string, data []byte) (err error) {
 //
 // It should give an error if the file is corrupted in any way.
 func (userdata *User) LoadFile(filename string) (data []byte, err error) {
-	return
+	var encrypted_file_data_and_signature [2][]byte
+	filename_json = json.Marshal(filename)
+	encrypt_filename = encrypt_data(userdate.encryptionKey, filename_json)
+
+	file_data_json,filename_exist = userlib.DatastoreGet(encrypt_filename)
+	if !filename_exist {
+		return nil, errors.New(strings.ToTitle("Wrong file name !!!"))
+	}
+	err := json.Unmashaled(file_data_json, &encrypted_file_data_and_signature)
+	if err != nil {
+		return nil, errors.New(strings.ToTitle("Json of file data corrupted !!!"))
+	}
+	encrypted_file_data = encrypted_file_data_and_signature[0]
+	signed_encrypted_file_data = encrypted_file_data_and_signature[1]
+	err := userlib.RSAVerify(signed_key.PublicKey, encrypted_file_data, signed_encrypted_file_data)
+	if err != nil {
+		return nil, errors.New(strings.ToTitle("file data corrupted !!!"))
+	}
+	file_json = decrypt_data(userdata.encyptionKey, encrypted_file_data)
+	var file_data string
+	err := json.Unmashaled(file_json, file_data) 
+	if err != nil {
+		return nil, errors.New(strings.ToTitle("Json of file data corrupted !!!"))
+	}
+	userdate.fileContent = file_data
+	return &userdata, err
 }
 
 // You may want to define what you actually want to pass as a
@@ -257,3 +338,16 @@ func (userdata *User) ReceiveFile(filename string, sender string,
 func (userdata *User) RevokeFile(filename string) (err error) {
 	return
 }
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
